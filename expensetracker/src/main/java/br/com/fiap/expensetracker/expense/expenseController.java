66621fa8cda5dd3ba68a6b9290b1454cb7964e63
FelipeRoveroni <rm95430@fiap.com.br>
2023-10-19 01:01:12 -0300

    Subindo att de estilização da pagina

--- expensetracker/src/main/java/br/com/fiap/expensetracker/expense/expenseController.java
+++ expensetracker/src/main/java/br/com/fiap/expensetracker/expense/expenseController.java
@@ -1,5 +1,7 @@
 package br.com.fiap.expensetracker.expense;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,7 @@ public class expenseController {
     
     @GetMapping
     public String index(Model model){
-        model.addAttribute("expenses", "Lista de Despesas");
+        model.addAttribute("expenses", List.of("Despesa 1", "Despesa 2", "Despesa 3"));
         return "expense/index";
     }
 }
