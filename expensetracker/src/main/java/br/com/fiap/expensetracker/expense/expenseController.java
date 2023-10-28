2e196f6ea723cf7b4bc43ef0109b95e7fde7b58e
FelipeRoveroni <rm95430@fiap.com.br>
2023-10-27 22:38:09 -0300

    Subindo atualização de Cadastro/login de Usuario

--- expensetracker/src/main/java/br/com/fiap/expensetracker/expense/expenseController.java
+++ expensetracker/src/main/java/br/com/fiap/expensetracker/expense/expenseController.java
@@ -1,6 +1,8 @@
 package br.com.fiap.expensetracker.expense;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,9 @@ public class expenseController {
     ExpenseService service;
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user){
+        model.addAttribute("avatar_url", user.getAttribute("avatar_url"));
+        model.addAttribute("username", user.getAttribute("name"));
         model.addAttribute("expense", service.findAll());
         return "expense/index";
     }
